703d0ded8a5064b1bf7ed251609ca590be78d88f
ek <ek@DESKTOP-99SGR1S>
2023-02-05 00:45:11 +0100

    Add project files.

---
+++ School.Data/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string ParentName { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string PlaceOfBirth { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public char Sex { get; set; }
+        [Required]
+        public int Student_ID { get; set; }
+        [Required]
+        public int ID_Number { get; set; }
+    }
+}
---
+++ School.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using School.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
